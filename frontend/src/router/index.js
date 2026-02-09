8fddfe50d5760bb8e72652cd919b2792b10ded53
Niklas Edstrom <Niklas.Edstrom@lpt.com>
2026-02-09 14:20:50 -0500

    Added template page and formatting to form page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,13 +4,15 @@ import SignupView from '../views/SignupView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import HouseMapView from '../views/HouseMapView.vue'
 import CreateMailView from '../views/CreateMailView.vue'
+import CreateMailTemplatesView from '../views/CreateMailTemplatesView.vue'
 
 const routes = [
     { path: '/', redirect: '/login' },
     { path: '/login', name: 'login', component: LoginView },
     { path: '/signup', name: 'signup', component: SignupView },
     { path: '/dashboard', name: 'dashboard', component: DashboardView },
-    { path: '/create-mail', name: 'create-mail', component: CreateMailView },
+    { path: '/create-mail', name: 'create-mail', component: CreateMailTemplatesView },
+    { path: '/create-mail/form', name: 'create-mail-form', component: CreateMailView },
     { path: '/map-test', name: 'map-test', component: HouseMapView }
 ]
 
